1f18512ec5047b9a89e9f4e0a51d9b0a53aff8a1
Rodrigo Naray <rodrigo.naray@fing.edu.uy>
2024-08-30 18:33:54 -0300

    Se agrega envio de email

--- src/config/envs.config.js
+++ src/config/envs.config.js
@@ -12,5 +12,7 @@ export default {
   SECRET_CODE: process.env.SECRET_CODE,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  JWT_SECRET_CODE:process.env.JWT_SECRET_CODE
+  JWT_SECRET_CODE:process.env.JWT_SECRET_CODE,
+  GMAIL_PASS: process.env.GMAIL_PASS,
+  GMAIL_EMAIL: process.env.GMAIL_EMAIL,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,15 @@ import { Router } from "express";
 import cartsRouter from "./carts.routes.js"
 import productsRouter from "./products.routes.js"
 import sessionRouter from "./session.routes.js"
+import contactRouter from "./contact.routes.js"
 
 const router = Router()
 
 router.use("/carts", cartsRouter)
 router.use("/products", productsRouter)
 router.use("/session", sessionRouter)
+router.use("/contact", contactRouter)
+
 router.get("*", async (req, res) => {
   try {
     res.status(404).json({ status: "error", msg: "Route not found" });
